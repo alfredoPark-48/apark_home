c4f12e4f812935cbe418a794ce1c1414baaac254
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-07-26 21:03:56 -0600

    Updated project structure

--- src/components/UI/Icons/CloseIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { FC } from 'react'
-
-const CloseIcon: FC = () => {
-	return <i className='big close red icon' />
-}
-
-export default CloseIcon
--- src/components/UI/Icons/HamIcon.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react'
-
-const HamIcon: FC = () => {
-	return <i className='big bars icon inverted' />
-}
-export default HamIcon
