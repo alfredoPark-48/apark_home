b7ae1231d8aaf66fb2346551a2388126f879a7b3
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-07-30 19:09:45 -0600

    Added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import MainLayout from '@/components/layout/MainLayout'
+import Image from 'next/image'
+import AlfredoImage from '../assets/images/alfredo_park.jpg'
 
 export default function Home() {
 	return (
 		<MainLayout>
-			<div className='flex flex-col'>
-				<div className='flex justify-center'>
-					<h1>Hello</h1>
+			<div className='flex justify-center p-10 gap-10'>
+				<div>
+					<Image
+						src={AlfredoImage.src}
+						width={AlfredoImage.width / 2}
+						height={AlfredoImage.height / 2}
+						alt='Alfredo image'
+						className='rounded-2xl'
+					/>
+				</div>
+				<div className='flex flex-col text-center items-center justify-center'>
+					<h1 className='text-3xl mb-2'>Hello there, I am Alfredo Park</h1>
+					<h2 className='text-xl'>A Software Developer</h2>
 				</div>
 			</div>
 		</MainLayout>
