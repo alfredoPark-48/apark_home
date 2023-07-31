25e4d4b564fd606e64af87b95ea9c6e9141ccd80
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-07-30 19:24:55 -0600

    Added responsive styling to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import AlfredoImage from '../assets/images/alfredo_park.jpg'
 export default function Home() {
 	return (
 		<MainLayout>
-			<div className='flex justify-center p-10 gap-10'>
+			<div className='flex flex-col sm:flex-row justify-center p-10 gap-10'>
 				<div>
 					<Image
 						src={AlfredoImage.src}
@@ -16,8 +16,26 @@ export default function Home() {
 					/>
 				</div>
 				<div className='flex flex-col text-center items-center justify-center'>
-					<h1 className='text-3xl mb-2'>Hello there, I am Alfredo Park</h1>
-					<h2 className='text-xl'>A Software Developer</h2>
+					<h1 className='text-3xl mb-2'>
+						Hello there, I am Alfredo Park
+					</h1>
+					<div className='flex items-center gap-2'>
+						<h2 className='text-xl'>A Software Developer</h2>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							strokeWidth={1.5}
+							stroke='currentColor'
+							className='w-6 h-6'
+						>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								d='M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5'
+							/>
+						</svg>
+					</div>
 				</div>
 			</div>
 		</MainLayout>
