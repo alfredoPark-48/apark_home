f951225557397b571317c6621dd08f2f6c7c2b44
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-07-31 21:37:07 -0600

    Routing handled by nextjs router

--- src/components/ui/NavigationBar/NavigationBar.tsx
+++ src/components/ui/NavigationBar/NavigationBar.tsx
@@ -1,35 +1,54 @@
 'use client'
 import { FC } from 'react'
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { useRouter } from 'next/navigation'
 
 const NavigationBar: FC = () => {
+	const router = useRouter()
+
+	const routeChangeHandler = (routeName: string): void => {
+		router.push(routeName)
+	}
+
 	return (
-		<Navbar
-			collapseOnSelect
-			expand='lg'
-			bg='dark'
-			data-bs-theme='dark'
-		>
+		<Navbar collapseOnSelect expand='lg' bg='dark' data-bs-theme='dark'>
 			<Container>
-				<Navbar.Brand href='/' className='uppercase text-white'>
+				<Navbar.Brand
+					className='uppercase text-white cursor-pointer'
+					onClick={() => routeChangeHandler('/')}
+				>
 					apark
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 				<Navbar.Collapse id='responsive-navbar-nav'>
 					<Nav className='me-auto'>
-						<Nav.Link href='/experience'>Experience</Nav.Link>
-						<Nav.Link href='/projects'>Projects</Nav.Link>
+						<Nav.Link
+							onClick={() => routeChangeHandler('/experience')}
+						>
+							Experience
+						</Nav.Link>
+						<Nav.Link
+							onClick={() => routeChangeHandler('/projects')}
+						>
+							Projects
+						</Nav.Link>
 						<NavDropdown
 							title='Other'
 							id='collapsible-nav-dropdown'
 						>
-							<NavDropdown.Item href='/leetcode'>
+							<NavDropdown.Item
+								onClick={() => routeChangeHandler('/leetcode')}
+							>
 								Leetcode
 							</NavDropdown.Item>
-							<NavDropdown.Item href='/books'>
+							<NavDropdown.Item
+								onClick={() => routeChangeHandler('/books')}
+							>
 								Books
 							</NavDropdown.Item>
-							<NavDropdown.Item href='/courses'>
+							<NavDropdown.Item
+								onClick={() => routeChangeHandler('/courses')}
+							>
 								Courses
 							</NavDropdown.Item>
 						</NavDropdown>
