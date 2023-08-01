6d001beb3865f519ba2bcb6b673e2df4aa1a808f
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-07-31 21:19:02 -0600

    Updated footer

--- src/components/ui/Footer/Footer.tsx
+++ src/components/ui/Footer/Footer.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react'
+import NextJsLogo from '@/assets/icons/nextjs-icon-svgrepo-com.svg'
+import Image from 'next/image'
 
 const Footer: FC = () => {
 	return (
-		<div className='text-center p-10'>
+		<div className='flex flex-col text-center p-10 gap-2'>
 			&copy; {new Date().getFullYear()} Alfredo Park
+			<div className='flex justify-center gap-2 items-center'>
+				<p className='text-sm'>Built with Next.js</p>
+				<Image src={NextJsLogo} width={20} height={20} alt='nextjs-logo' />
+			</div>
 		</div>
 	)
 }
