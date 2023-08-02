6fa9f3a9ee6d6bfc327207793fff12d81f9cc702
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-08-01 21:57:22 -0600

    Updated experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react'
 import MainLayout from '@/components/layout/MainLayout'
-import ExperienceCard from '@/components/ui/ExperienceCard/ExperienceCard'
+import ExperienceCard from '@/components/ui/Experience/ExperienceCard/ExperienceCard'
 import ListoMxLogo from '@/assets/images/listo-logo.png'
 import RubidexLogo from '@/assets/images/rubidex-logo.png'
+import ExperienceItem from '@/components/ui/Experience/ExperienceItem/ExperienceItem'
 
 const ExperiencePage: FC = () => {
 	return (
@@ -16,20 +17,7 @@ const ExperiencePage: FC = () => {
 						date='March 2023 - Present'
 						logo={RubidexLogo.src}
 					>
-						○ Extracted data from account statements using Django
-						and Regular language to generate a personalized summary
-						in PDF format. 
-						○ Extracted information from invoices to
-						be displayed in the interface using Django. 
-						○ Extracted
-						Bill of Lading data from invoices using Django and Scala
-						to generate a summary in Excel format. 
-						○ Implemented
-						tests to ensure code quality and detect errors during
-						deployment. 
-						○ Updated the installation process for the
-						development environment for future developers in the
-						team.
+						<ExperienceItem key='rubidex-1'></ExperienceItem>
 					</ExperienceCard>
 					<ExperienceCard
 						key={0}
@@ -37,11 +25,33 @@ const ExperiencePage: FC = () => {
 						date='November 2021 - May 2022'
 						logo={ListoMxLogo.src}
 					>
-						Lorem ipsum dolor sit amet, consectetur adipisicing
-						elit. Minima inventore architecto explicabo animi ab
-						repellendus commodi. Dolores praesentium ad distinctio
-						asperiores. Beatae illum fuga, laboriosam dignissimos
-						nam maiores placeat reprehenderit?
+						<ExperienceItem
+							key='listo-1'
+							technologies='Django, RegEx'
+						>
+							Extracted data from account statements to generate a
+							personalized summary in PDF format.
+						</ExperienceItem>
+						<ExperienceItem key='listo-2' technologies='Django'>
+							Extracted information from invoices to be displayed
+							in the user interface.
+						</ExperienceItem>
+						<ExperienceItem
+							key='listo-3'
+							technologies='Django, Scala'
+						>
+							Extracted Bill of Lading data from invoices using
+							Django and Scala to generate a summary in Excel
+							format.
+						</ExperienceItem>
+						<ExperienceItem key='listo-4'>
+							Implemented tests to ensure code quality and detect
+							errors during deployment.
+						</ExperienceItem>
+						<ExperienceItem key='listo-5' technologies=''>
+							Updated the installation process for the development
+							environment for future developers in the team.
+						</ExperienceItem>
 					</ExperienceCard>
 				</div>
 			</div>
