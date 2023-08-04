064d1f7f8d7a3f25746b3db650c3b2813cb1a90f
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-08-03 23:34:00 -0600

    Added projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import MainLayout from '@/components/layout/MainLayout'
+
+const ProjectPage: FC = () => {
+	return (
+		<MainLayout>
+			<h1>Hello</h1>
+		</MainLayout>
+	)
+}
+
+export default ProjectPage
