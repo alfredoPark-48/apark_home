f3b5a332c38388a9d65548fac942db7c473c368e
Alfredo Park <parkalfredojeonghyun@gmail.com>
2023-08-10 00:10:43 -0600

    Added BookItem component and updated page

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,16 +1,26 @@
+'use client'
+
 import { FC } from 'react'
 import MainLayout from '@/components/layout/MainLayout'
+import BookItem from '@/components/ui/BookItem/BookItem'
 
 const BooksPage: FC = () => {
 	return (
 		<MainLayout>
 			<div className='flex flex-col p-10'>
-				<h1 className='text-2xl'>Books</h1>
-				<p>
+				<h1 className='text-3xl'>Books</h1>
+				<p className='text-lg'>
 					The following books are some that have helped me throughout
 					my development.
 				</p>
-				<div className='flex'></div>
+				<div className='flex'>
+					<BookItem
+						bookImage='https://m.media-amazon.com/images/I/81yvVmN1BLL._AC_UF1000,1000_QL80_.jpg'
+						productLink=''
+						title='Books'
+						description='book'
+					/>
+				</div>
 			</div>
 		</MainLayout>
 	)
